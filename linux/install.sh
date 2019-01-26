b3815e418eb54906db54f1b6d4b71a3607ae13fe
Ben Simmonds <ben@bencode.net>
2019-01-26 14:11:32 +1100

    fresh git aliases, and made install.sh more robust

--- linux/install.sh
+++ linux/install.sh
@@ -4,24 +4,41 @@
 # On a fresh machine, these are actual files, so replace them.
 # If they are already symlinks, don't touch them.
 replace_file_with_symlink() {
-  if [[ ! -n $1 ]]; then echo "no arg1: target file"; return 1; fi
-  if [[ ! -n $2 ]]; then echo "no arg2: source file"; return 2; fi
-  if [[ ! -e ~/git/scripts ]]; then echo "~/git/scripts doesn't exist"; return 3; fi
 
-  if [[ ! -L $1 ]]; then
-    echo "linking $1 to $2"
-    rm $1
-    ln -nfs $2 $1
+  gitpath=~/git
+  scriptspath="$gitpath/scripts"
+  srcfile=$1
+  destfile=$2
+
+  if [[ ! -n $destfile ]]; then echo "no arg1: target file"; return 1; fi
+  if [[ ! -n $srcfile ]]; then echo "no arg2: source file"; return 2; fi
+  if [[ ! -d ~/git/scripts ]]; then
+    echo "$scriptpath doesn't exist, attempt to clone git repo..."
+    if ! mkdir -p $gitpath && git clone git@github.com:bm4cs/scripts.git $gitpath; then
+      echo "failed to clone git repo, aborting";
+      return 3; 
+    fi
+  fi
+
+  if [[ ! -L $destfile ]]; then
+    echo "linking $destfile to $srcfile"
+
+    if [[ -e $destfile ]]; then
+      rm $destfile
+    fi 
+
+    ln -nfs $srcfile $destfile
   else
-    echo "$1 already exists and is a symlink, nothing to do"
+    echo "$destfile already exists and is a symlink, nothing to do"
   fi
 }
 
 
-replace_file_with_symlink ~/.vimrc ~/git/scripts/linux/vim/vimrc
-replace_file_with_symlink ~/.gitconfig ~/git/scripts/linux/git/gitconfig
-replace_file_with_symlink ~/.bashrc ~/git/scripts/linux/bash/bashrc
-replace_file_with_symlink ~/.bash_aliases ~/git/scripts/linux/bash/aliases
-replace_file_with_symlink ~/.muttrc ~/git/scripts/linux/mutt/.muttrc
-replace_file_with_symlink ~/.mutt ~/git/scripts/linux/mutt/.mutt
+replace_file_with_symlink ~/git/scripts/linux/vim/vimrc ~/.vimrc 
+replace_file_with_symlink ~/git/scripts/linux/git/gitconfig ~/.gitconfig
+replace_file_with_symlink ~/git/scripts/linux/bash/bashrc ~/.bashrc
+replace_file_with_symlink ~/git/scripts/linux/bash/profile ~/.profile
+replace_file_with_symlink ~/git/scripts/linux/bash/aliases ~/.bash_aliases
+replace_file_with_symlink ~/git/scripts/linux/mutt/.muttrc ~/.muttrc
+replace_file_with_symlink ~/git/scripts/linux/mutt/.mutt ~/.mutt
 
