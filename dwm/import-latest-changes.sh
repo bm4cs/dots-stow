542167119984061615fe0aea7a2272de4166843e
Ben Simmonds <ben@bencode.net>
2020-01-27 20:11:12 +1100

    dwm apply patch script

--- dwm/import-latest-changes.sh
+++ dwm/import-latest-changes.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 if [ ! -f dwm-mods.patch ]; then
-    echo "Patch not found, aborting"
+	echo "Patch not found, aborting"
 	exit
+fi
+
+if [ ! -d ~/git/dwm ]; then
+	git clone git://git.suckless.org/dwm ~/git/dwm
 fi
 
 cp -f dwm-mods.patch ~/git/dwm
